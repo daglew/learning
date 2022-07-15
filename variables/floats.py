ecf7d91e51407070f4f65955833092fef50b14bc
daglew <daga122611@gmail.com>
2022-07-15 23:31:01 +0200

    floats

--- variables/floats.py
+++ variables/floats.py
@@ -1,1 +1,22 @@
 moj_float = 5.55
+
+# zaokragla na koncu zera do jednego miejsca po przecinku (TYLKO ZERA)
+
+x = float("3.500")
+print(x)
+
+ # sprawdzam instancje zwraca True lub False
+sprawdzam_instancje = isinstance(moj_float, float)
+print(sprawdzam_instancje)
+
+# sprawdzam typ
+sprawdzam_typ = type(moj_float)
+print(sprawdzam_typ)
+
+#  robie f""
+przykladowy_fstr = f"Tutaj wklejam zmienna moj float: {moj_float}"
+print(przykladowy_fstr)
+
+#  przekonwertowanie liczby na liczbe zmienno przecinkowa
+moj_float1 = float(55)
+print(moj_float1)
