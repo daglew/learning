02c7cfa10c3867549780b405555a14de9d5a21f4
daglew <daga122611@gmail.com>
2022-07-26 22:49:06 +0200

    unit_tests

--- variables/try_except_wyjatki.py
+++ variables/try_except_wyjatki.py
@@ -13,15 +13,15 @@
 # except AssertionError as ex:
 #     print(ex)
 
-# wyrzucanie wyjatku
-lista1 = [6]
-string1 = "oko"
-
-try:
-    assert lista1 == string1, f"Wartosc lista1: {lista1}, nie jest wartosci string1: {string1}"
-except AssertionError as hgfgnfnfnnfnfnfn:
-    raise Exception(f"Przechwycilam wyjatek hgfgnfnfnnfnfnfn: {hgfgnfnfnnfnfnfn}, przy sprawdaniu asserci wartosci "
-                    f"lista1: {lista1}, oraz wartosci string1: {string1}")
+# # wyrzucanie wyjatku
+# lista1 = [6]
+# string1 = "oko"
+#
+# try:
+#     assert lista1 == string1, f"Wartosc lista1: {lista1}, nie jest wartosci string1: {string1}"
+# except AssertionError as hgfgnfnfnnfnfnfn:
+#     raise Exception(f"Przechwycilam wyjatek hgfgnfnfnnfnfnfn: {hgfgnfnfnnfnfnfn}, przy sprawdaniu asserci wartosci "
+#                     f"lista1: {lista1}, oraz wartosci string1: {string1}")
 # druga_zmienna = "oko"
 #
 # # printowanie wyjatku
@@ -57,41 +57,34 @@ except AssertionError as hgfgnfnfnnfnfnfn:
 #
 # funkcjia_ktora_przyjmuje_zmienna(zmienna_float)
 
-""""
-funkcja przyjmuje list
-sprawdz czy lista ma wartosc w srodku
-jesli niema wyrzuc wyjatek
-nastepnie przeiteruj po wartosciach - for
-zamien je w str 
-i dodaj xd
-zwroc liste z nowymi wartosciami
-exception (podana lista jest pusta)
-"""
-
-lista_zmienna = []
-lista_ze_zmiennymi = [10, "rrr", 1.45, (5, 89), {"a": 23}]
-
-
-def funkcja_przyjmuje_list(lista):
-    if lista == []:
-        raise Exception(f"Podana lista: {lista} jest pusta")
-
-    lista_z_nowymi_wartosciami = []
-    for element in lista:
-        string_plus_xd = f"{element}xd"
-        lista_z_nowymi_wartosciami.append(string_plus_xd)
-
-    list_comprehension = [f"{element}xd" for element in lista]
-
-
-    return lista_z_nowymi_wartosciami, list_comprehension
-
-
-
-
-# funkcja_przyjmuje_list(lista=lista_zmienna)
-lista_z_nowymi_wartosciami, list_comprehension = funkcja_przyjmuje_list(lista=lista_ze_zmiennymi)
-print(lista_z_nowymi_wartosciami)
-print(list_comprehension)
-
-
+# """"
+# funkcja przyjmuje list
+# sprawdz czy lista ma wartosc w srodku
+# jesli niema wyrzuc wyjatek
+# nastepnie przeiteruj po wartosciach - for
+# zamien je w str
+# i dodaj xd
+# zwroc liste z nowymi wartosciami
+# exception (podana lista jest pusta)
+# """
+#
+# lista_zmienna = []
+# lista_ze_zmiennymi = [10, "rrr", 1.45, (5, 89), {"a": 23}]
+#
+#
+# def funkcja_przyjmuje_list(lista):
+#     if lista == []:
+#         raise Exception(f"Podana lista: {lista} jest pusta")
+#
+#     lista_z_nowymi_wartosciami = []
+#     for element in lista:
+#         string_plus_xd = f"{element}xd"
+#         lista_z_nowymi_wartosciami.append(string_plus_xd)
+#
+#     list_comprehension = [f"{element}xd" for element in lista]
+#
+#
+#     return lista_z_nowymi_wartosciami, list_comprehension
+#
+#
+#
