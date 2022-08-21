99181b93f2ab5771ad5cea5fad21d095f5486f0d
daglew <daga122611@gmail.com>
2022-08-21 23:38:32 +0200

    os_package_1

--- praca_na_plikach_os/os_package(funkcje_systemowe).py
+++ praca_na_plikach_os/os_package(funkcje_systemowe).py
@@ -1,29 +1,33 @@
 # https://www.geeksforgeeks.org/os-module-python-examples/
 import os
+#
+# #  zwraca aktualna sciezke pracy CWD
+# cwd = os.getcwd()
+# print(f"To jest aktualna sciezka pracy: {cwd}.")
+#
+#
+#
+# #  zmiana aktualnej sciezki
+#
+# def current_path():
+#     print("Current working directory before")
+#     print(os.getcwd())
+#     print()
+#
+#
+# current_path()
+# os.chdir("../")
+# current_path()
 
-#  zwraca aktualna sciezke pracy CWD
-cwd = os.getcwd()
-print(f"To jest aktualna sciezka pracy: {cwd}.")
-
-
-
-#  zmiana aktualnej sciezki
-
-def current_path():
-    print("Current working directory before")
-    print(os.getcwd())
-    print()
-
-
-current_path()
-os.chdir("../")
-current_path()
-
-# dodanie pliku
+# stworzenie pliku
+cwd_1 = os.getcwd()
+print(cwd_1)
+os.mkdir("new_dir")
 
 # usuniecie pliku
+os.rmdir(f"{cwd_1}\\new_dir")
 
-# sprawdzenie uprawnien pliku
+# nadawniae uprawnień uprawnien pliku
 
 # sprawdzenie czy cos jest plikiem
 
