38d9a388cc7baf24854a882b02d0a5be820c9bfb
daglew <daga122611@gmail.com>
2022-08-22 23:01:49 +0200

    os_package_2

--- praca_na_plikach_os/os_package(funkcje_systemowe).py
+++ praca_na_plikach_os/os_package(funkcje_systemowe).py
@@ -1,5 +1,7 @@
 # https://www.geeksforgeeks.org/os-module-python-examples/
 import os
+import stat
+
 #
 # #  zwraca aktualna sciezke pracy CWD
 # cwd = os.getcwd()
@@ -20,6 +22,7 @@ import os
 # current_path()
 
 # stworzenie pliku
+
 cwd_1 = os.getcwd()
 print(cwd_1)
 os.mkdir("new_dir")
@@ -28,7 +31,27 @@ os.mkdir("new_dir")
 os.rmdir(f"{cwd_1}\\new_dir")
 
 # nadawniae uprawnień uprawnien pliku
+uprawnienie = stat.S_IRWXU
+plik = "D:\\repositories\\learning\\praca_na_plikach_os\\new_txt.txt"
+os.chmod(plik, uprawnienie)
 
 # sprawdzenie czy cos jest plikiem
+plik_1 = "D:\\repositories\\learning\\praca_na_plikach_os\\new_txt.txt"
+sprawdzenie = os.path.isfile(path=plik_1)
+print(sprawdzenie)
 
 # sprawdzenie czy cos jest folderem
+folder_1 = "D:\\repositories\\learning\\praca_na_plikach_os"
+sprawdzenie = os.path.isdir(folder_1)
+print(sprawdzenie)
+
+# sprawdzenie czy w ogóle path istnieje
+plik_2 = "D:\\repositories\\learning\\praca_na_plikach_os\\new_txt.txt"
+sprawdzenie = os.path.exists(path=plik_1)
+print(sprawdzenie)
+
+# listowanie plików z folderu
+folder_2 = "D:\\repositories\\learning\\praca_na_plikach_os"
+lista_plikow = os.listdir(folder_2)
+print(lista_plikow)
+
