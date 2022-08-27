4bcf788ef841bd0e286258b3cb15981a6534564f
daglew <daga122611@gmail.com>
2022-08-27 23:07:02 +0200

    list_comprehension

--- variables/list_comprehension_1.py
+++ variables/list_comprehension_1.py
@@ -3,7 +3,7 @@ lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # stworz nowa liste z listy liczb jesli liczba jest podzielna przez 2
 
-lista_1 = [liczba for liczba in lista_liczb if not liczba % 2]
+lista_1 = [liczba for liczba in lista_liczb if liczba % 2 == 0]
 print()
 
 # stworz nowa liste z kazdym elementem pomnozonym przez 2 z listy liczb
@@ -17,3 +17,30 @@ print()
 # stworz liste z listy liczb ktora bedzie zawierala liczby pomnozone przez 5 jesli liczba jest niearzysta jesli liczba jest wieksza od 3
 lista_4 = [liczba * 5 for liczba in lista_liczb if liczba % 2 if liczba > 3]
 print()
+
+
+# result = ['h', 'u', 'm', 'a', 'n']
+string1 = 'human'
+human = [e for e in string1]
+print(human)
+
+# liczby w zakresie do 20 ale tylko parzyste (w zakresie in range(liczba))
+# result [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+zakres = [a for a in range(0, 20) if a % 2 == 0]
+print(zakres)
+
+# result ['Chb', 'Tdb']
+# use lower().endswith('b'), len(name) > 2
+names = ['Ch', 'Dh', 'Eh', 'cb', 'Tb', 'Td', 'Chb', 'Tdb']
+names_lc = [b for b in names if b.lower().endswith('b') and len(names) > 2]
+print()
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# sprawdź czy słowo ma a w wyrazie
+# result ['apple', 'banana', 'mango']
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# result ["Apple", "Banana", "Cherry", "Kiwi", "Mango"]
+
+# lista = [x, y, x, y, x, y, x, y]
+# result = [iks, igrek, iks, igrek, iks, igrek, iks, igrek]
