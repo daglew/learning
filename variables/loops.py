75def70b1981eec245e6e2d2f4c045aca4634763
daglew <daga122611@gmail.com>
2022-08-31 22:49:32 +0200

    loops_1

---
+++ variables/loops.py
@@ -0,0 +1,45 @@
+# pętla for dla listy [1,2,3,4,5,6, True, False, []]
+# wyprintuj
+a = [1, 2, 3, 4, 5, 6, True, False, []]
+for element in a:
+    print(element)
+
+# pętla for dla listy [1,2,3,4,5,6, True, False, []]
+# element zamień na stringa dodaj "str"
+# stwórz nową listę dodaj element
+
+pusta = []
+for elementt in a:
+    element_na_str = pusta.append(str(elementt))
+print(pusta)
+
+
+# pętla for in range 1-20
+# wyprintuj
+
+for elemen in range(1, 21):
+    print(elemen)
+
+print()
+# pętla z enumerate(licznikiem) dla listy [1,2,3,4,5,6, True, False, []]
+# zobacz -> https://realpython.com/python-enumerate/
+# wyprintuj
+a = [1, 2, 3, 4, 5, 6, True, False, []]
+
+for elem, aa in enumerate(a):
+    print(elem, aa)
+
+# pętla dla dicta={1:2, 2:4, 3:5, 6:7, 7:8, 8:9, 10:15}
+# wyprintuj
+
+
+# pętla dla kluczy z dicta={1:2, 2:4, 3:5, 6:7, 7:8, 8:9, 10:15}
+# wyprintuj
+
+# pętla dla wartości z dicta={1:2, 2:4, 3:5, 6:7, 7:8, 8:9, 10:15}
+# wyprintuj
+
+# pętla while
+# zobacz -> https://analityk.edu.pl/python-petla-for-oraz-while/ przerób przykłady
+
+# niekończąca się petla while True
